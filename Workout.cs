6a03b29fadd01dae1f202892cc2acebc00bc0493
Romain Richards <Romain.Richards@outlook.com>
2023-08-16 20:42:38 -0400

    Create workout class with properties to get and set data for each workout.

---
+++ Workout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_Application_Tracker
+{
+    partial class WorkoutTable 
+    {
+        public string Abs { get; set; } 
+        public string LowerBody1 { get; set; }
+        public string UpperBody1 { get; set; }
+    }
+}
